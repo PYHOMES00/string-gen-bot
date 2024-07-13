55b7468d9d7c938ee3603c33d1a5abc015f55464
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-13 21:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from bot import Bot
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Tech VJ'
-
+def home():
+    return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run()
-  
+    bot = Bot()
+    bot.run()  # Assuming this doesn't block
+    app.run(host='0.0.0.0', port=8080)  # Bind to port 5000
