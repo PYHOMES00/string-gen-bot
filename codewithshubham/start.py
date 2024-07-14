b6d2349a517c43675975a71790c8d67c8c1be6b4
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-14 17:11:50 +0530

    Update start.py

--- codewithshubham/start.py
+++ codewithshubham/start.py
@@ -1,14 +1,19 @@
+from codewithshubham.must_join import must_join
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from config import OWNER_ID
-
 
 def filter(cmd: str):
     return filters.private & filters.incoming & filters.command(cmd)
 
 @Client.on_message(filter("start"))
 async def start(bot: Client, msg: Message):
+    # Check for force subscription
+    fsub_status = await must_join(bot, msg)
+    if fsub_status == 400:
+        return
+
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
@@ -22,10 +27,11 @@ async def start(bot: Client, msg: Message):
 ᴍᴀᴅᴇ ʙʏ  : [ᴛᴇᴀᴍ ᴄʀᴇᴀᴛᴏʀ](t.me/the_creator_botz) !""",
         reply_markup=InlineKeyboardMarkup(
             [
-              [
+                [
                     InlineKeyboardButton("ᴀᴅᴍɪɴ 1", url="https://t.me/shubham_X_Official"),
                     InlineKeyboardButton("ᴀᴅᴍɪɴ 2", url="https://t.me/shubham_X_Official")
-              ],[
+                ],
+                [
                     InlineKeyboardButton(text="ɢᴇɴʀᴀᴛᴇ ꜱᴛʀɪɴɢ", callback_data="generate")
                 ],
                 [
